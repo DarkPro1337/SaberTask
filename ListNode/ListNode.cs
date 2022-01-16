0f4feec4450d8bde6b751a48329fd18249a4f7e1
Artem Darkov <darkprothekun@gmail.com>
2022-01-16 15:17:40 +0300

    Serialize method added

--- ListNode/ListNode.cs
+++ ListNode/ListNode.cs
@@ -20,7 +20,37 @@ namespace ListNode
 
         public void Serialize(Stream s)
         {
+            var nodeList = new List<ListNode>();
+            var currentNode = Head;
 
+            while (currentNode != null)
+            {
+                nodeList.Add(currentNode);
+                currentNode = currentNode.Next;
+            }
+
+            if (nodeList.Count != Count)
+            {
+                throw new Exception("Structure of ListRand object and Count aren't equal!");
+            }
+
+            using (BinaryWriter bw = new BinaryWriter(s))
+            {
+                bw.Write(Count);
+
+                foreach (var node in nodeList)
+                {
+                    bw.Write(node.Data);
+                    int position = -1;
+
+                    if (node.Random != null)
+                    {
+                        position = nodeList.IndexOf(node.Random);
+                    }
+
+                    bw.Write(position);
+                }
+            }
         }
 
         public void Deserialize(Stream s)
