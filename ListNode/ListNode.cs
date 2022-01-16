85832bb92aae7b27311e2393b84ebcce76ea79e7
Artem Darkov <darkprothekun@gmail.com>
2022-01-16 14:55:18 +0300

    ListNode class template added

---
+++ ListNode/ListNode.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace ListNode
+{
+    class ListNode
+    {
+        public ListNode Previous;
+        public ListNode Next;
+        public ListNode Random; // произвольный элемент внутри списка
+        public string Data;
+    }
+
+    class ListRandom
+    {
+        public ListNode Head;
+        public ListNode Tail;
+        public int Count;
+
+        public void Serialize(Stream s)
+        {
+
+        }
+
+        public void Deserialize(Stream s)
+        {
+
+        }
+    }
+}
