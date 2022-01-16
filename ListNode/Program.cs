e1003bd8606a3d82dabf603599ea136c44bb475a
Artem Darkov <darkprothekun@gmail.com>
2022-01-16 17:29:36 +0300

    Program finished

--- ListNode/Program.cs
+++ ListNode/Program.cs
@@ -6,7 +6,61 @@ namespace ListNodeTask
     {
         static void Main(string[] args)
         {
-            
+            // First List
+            var firstList = new ListNode
+            {
+                Data = "firstObject"
+            };
+
+            // Second List
+            var secondList = new ListNode
+            {
+                Data = "secondObject",
+                Previous = firstList,
+                Random = firstList
+            };
+            firstList.Next = secondList;
+
+            // Third List
+            var thirdList = new ListNode
+            {
+                Data = "thirdObject",
+                Previous = secondList,
+                Random = firstList
+            };
+            secondList.Next = thirdList;
+
+            // Fourth List
+            var fourthList = new ListNode
+            {
+                Data = "fourthObject",
+                Previous = thirdList,
+                Random = secondList
+            };
+            thirdList.Next = fourthList;
+
+            // Random
+            ListRandom randomList = new ListRandom
+            {
+                Count = 4,
+                Head = firstList,
+                Tail = fourthList
+            };
+
+            // Serialize
+            using (FileStream fileStream = new FileStream("DATA.dat",
+                    FileMode.OpenOrCreate, FileAccess.Write, FileShare.None))
+            {
+                randomList.Serialize(fileStream);
+            }
+
+            // Deserialize
+            var resource = new ListRandom();
+            using (FileStream fileStream = new FileStream("DATA.dat",
+                    FileMode.Open, FileAccess.Read, FileShare.None))
+            {
+                resource.Deserialize(fileStream);
+            }
         }
     }
 }
