bff9b4ebeb569bd0577ba3f438b7e6dd89dd47ed
Artem Darkov <darkprothekun@gmail.com>
2022-01-16 15:58:02 +0300

    Deserialize method added
    Also added some comments

--- ListNode/ListNode.cs
+++ ListNode/ListNode.cs
@@ -6,18 +6,40 @@ namespace ListNode
 {
     class ListNode
     {
+        /// <summary>
+        /// Sequence element that is previous to the current one.
+        /// </summary>
         public ListNode Previous;
+        /// <summary>
+        /// Sequence element that is next to the current one.
+        /// </summary>
         public ListNode Next;
-        public ListNode Random; // произвольный элемент внутри списка
+        /// <summary>
+        /// Referencing an random element inside a list 
+        /// </summary>
+        public ListNode Random;
         public string Data;
     }
 
     class ListRandom
     {
+        /// <summary>
+        /// Node at the start of a list.
+        /// </summary>
         public ListNode Head;
+        /// <summary>
+        /// Node at the end of a list.
+        /// </summary>
         public ListNode Tail;
+        /// <summary>
+        /// Nodes count in a list.
+        /// </summary>
         public int Count;
 
+        /// <summary>
+        /// Serializes the current object.
+        /// </summary>
+        /// <param name="s">Filestream to which the serialized object will be written.</param>
         public void Serialize(Stream s)
         {
             var nodeList = new List<ListNode>();
@@ -53,9 +75,48 @@ namespace ListNode
             }
         }
 
+        /// <summary>
+        /// Deserializes from a file, overwriting the data of the current object.
+        /// </summary>
+        /// <param name="s">Filestream from which the serialized object will be read.</param>
         public void Deserialize(Stream s)
         {
+            try
+            {
+                using (BinaryReader br = new BinaryReader(s))
+                {
+                    Count = br.ReadInt32();
 
+                    var nodeList = new List<ListNode>(Count);
+
+                    for (int i = 0; i < Count; i++)
+                    {
+                        nodeList.Add(new ListNode());
+                    }
+
+                    for (int i = 0; i < Count; i++)
+                    {
+                        nodeList[i].Previous = i > 0 ? nodeList[i - 1] : null;
+                        nodeList[i].Next = i < (Count - 1) ? nodeList[i + 1] : null;
+                        nodeList[i].Data = br.ReadString();
+
+                        int randIdx = br.ReadInt32();
+
+                        nodeList[i].Random = randIdx >= 0 ? nodeList[randIdx] : null;
+                    }
+
+                    Head = nodeList[0];
+                    Tail = nodeList[Count - 1];
+                }
+            }
+            catch (Exception ex) when (ex is ObjectDisposedException ||
+                                       ex is EndOfStreamException ||
+                                       ex is IOException)
+            {
+                Count = 0;
+                Head  = null;
+                Tail  = null;
+            }
         }
     }
 }
--- ListNode/Program.cs
+++ ListNode/Program.cs
@@ -6,7 +6,7 @@ namespace ListNode
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
         }
     }
 }
